127af7f528f87c1e9eb60aa4e47ef9a0c1f8178c
Tamil Nesan <m2tamilnesan@gmail.com>
2021-08-08 22:04:05 +0530

    Exception Handler Added

---
+++ src/main/java/com/jwtreact/jwtreactintegration/common/Constant.java
@@ -0,0 +1,9 @@
+package com.jwtreact.jwtreactintegration.common;
+
+public interface Constant {
+
+    interface USER_TYPE{
+        String NORMAL = "NORMAL";
+        String ADMIN = "ADMIN";
+    }
+}
